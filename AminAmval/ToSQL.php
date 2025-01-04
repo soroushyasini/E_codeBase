8190d7965f09acafdebb4633438e56ac6c1c3e29
Soroush Yasini <soroush.yasini@outlook.com>
2025-01-04 12:08:12 +0330

    quaring talbe columns and initializing

---
+++ AminAmval/ToSQL.php
@@ -0,0 +1,21 @@
+<?
+// before get started, we should know column names,
+// here is SQL code to do so :
+    //SELECT COLUMN_NAME
+    //FROM INFORMATION_SCHEMA.COLUMNS
+    //WHERE TABLE_SCHEMA = 'your_database_name' AND TABLE_NAME = 'your_table_name';
+// her is column names :
+// APP_NUMBER
+// APP_STATUS
+// APP_UID
+// GROUH
+// MAKAN
+// MOSHAKHASE
+// ONVAN_AMVAL
+// PELAK_AMVAL
+// SANAD
+// TAHVIL_GIRANDE
+// TARIKH
+// TEDAD
+// TYPE
+// VASZIYAT_ESTEFADE
