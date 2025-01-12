bf3fd7e12c4fbcb8bfc6ab6bfa9511a86f121f54
Soroush Yasini <soroush.yasini@outlook.com>
2025-01-12 11:23:08 +0330

    minor midifs

--- monaghesat/xcrud.php
+++ monaghesat/xcrud.php
@@ -2,7 +2,6 @@
 libXcrudLoad();
 $xcrud = Xcrud::get_instance();
 $xcrud->table('PRC_DB_MOZAYEDAT_MONAGHESAT');
-
 $xcrud->columns(array('ID', 'insert_date', 'NAME', 'TYPE', 'MOZAYEDE_SHOMARE', 'DASTGAHEJRAI', 'DASTGAHEJRAI', 'MABLAGH', 'DEADLINE_ASNAD', 'DEADLINE_PASOKH', 'TAHVIL_BAR', 'ZEMANAT_NAMEH', 'ZEMANAT_NAMEH_2', 'AKHZ','VAHED_MARBUTE'));
 //$xcrud->columns('GROUH, ONVAN_AMVAL');
 //$xcrud->columns('NAME, TYPE, MOZAYEDE_SHOMARE, DASTGAHEJRAI, DASTGAHEJRAI, MABLAGH, n2_date(DEADLINE_ASNAD), DEADLINE_PASOKH, TAHVIL_BAR, ZEMANAT_NAMEH, ZEMANAT_NAMEH_2, AKHZ');
@@ -19,14 +18,14 @@ $xcrud->label('TAHVIL_BAR','نحوه تحویل');
 $xcrud->label('ZEMANAT_NAMEH','ضمانت‌نامه');
 $xcrud->label('ZEMANAT_NAMEH_2','ضمانت‌نامه ثانویه');
 $xcrud->label('AKHZ','منبع');
-$xcrud->label('tamin_konnande','تامین کننده');s
+$xcrud->label('TAMIN_KONNANDEt','تامین کننده');
 $xcrud->label('VAHED_MARBUTE','واحد');
 
-
+$xcrud->unset_numbers();
 // Enable delete button
 $xcrud->button('javascript:;', 'Delete', 'fa fa-trash', 'btn btn-danger', array(
     'data-task' => 'delete',
-    'data-primary' => '{IDs}', // Use the unique identifier 'APP_UID'
+    'data-primary' => '{ID}', // Use the unique identifier 'APP_UID'
 ));
 // Set your table or view from main database (you can write your query directly)
 $xcrud->unset_remove(false);
