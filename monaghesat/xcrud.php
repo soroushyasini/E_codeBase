e27bb4888ba8646a02686962ee5385c5f2936345
Soroush Yasini <soroush.yasini@outlook.com>
2025-01-12 11:02:41 +0330

    update trigger for new SQL fields, change primery key for deletation

--- monaghesat/xcrud.php
+++ monaghesat/xcrud.php
@@ -3,9 +3,11 @@ libXcrudLoad();
 $xcrud = Xcrud::get_instance();
 $xcrud->table('PRC_DB_MOZAYEDAT_MONAGHESAT');
 
-$xcrud->columns(array('NAME', 'TYPE', 'MOZAYEDE_SHOMARE', 'DASTGAHEJRAI', 'DASTGAHEJRAI', 'MABLAGH', 'DEADLINE_ASNAD', 'DEADLINE_PASOKH', 'TAHVIL_BAR', 'ZEMANAT_NAMEH', 'ZEMANAT_NAMEH_2', 'AKHZ','VAHED_MARBUTE'));
+$xcrud->columns(array('ID', 'insert_date', 'NAME', 'TYPE', 'MOZAYEDE_SHOMARE', 'DASTGAHEJRAI', 'DASTGAHEJRAI', 'MABLAGH', 'DEADLINE_ASNAD', 'DEADLINE_PASOKH', 'TAHVIL_BAR', 'ZEMANAT_NAMEH', 'ZEMANAT_NAMEH_2', 'AKHZ','VAHED_MARBUTE'));
 //$xcrud->columns('GROUH, ONVAN_AMVAL');
 //$xcrud->columns('NAME, TYPE, MOZAYEDE_SHOMARE, DASTGAHEJRAI, DASTGAHEJRAI, MABLAGH, n2_date(DEADLINE_ASNAD), DEADLINE_PASOKH, TAHVIL_BAR, ZEMANAT_NAMEH, ZEMANAT_NAMEH_2, AKHZ');
+$xcrud->label('ID','ID');
+$xcrud->label('insert_date','تاریخ ثبت');
 $xcrud->label('NAME','عنوان ');
 $xcrud->label('TYPE','نوع ');
 $xcrud->label('MOZAYEDE_SHOMARE','شماره مزایده/مناقصه');
@@ -17,13 +19,14 @@ $xcrud->label('TAHVIL_BAR','نحوه تحویل');
 $xcrud->label('ZEMANAT_NAMEH','ضمانت‌نامه');
 $xcrud->label('ZEMANAT_NAMEH_2','ضمانت‌نامه ثانویه');
 $xcrud->label('AKHZ','منبع');
+$xcrud->label('tamin_konnande','تامین کننده');s
 $xcrud->label('VAHED_MARBUTE','واحد');
 
 
 // Enable delete button
 $xcrud->button('javascript:;', 'Delete', 'fa fa-trash', 'btn btn-danger', array(
     'data-task' => 'delete',
-    'data-primary' => '{APP_UID}', // Use the unique identifier 'APP_UID'
+    'data-primary' => '{ID}', // Use the unique identifier 'APP_UID'
 ));
 // Set your table or view from main database (you can write your query directly)
 $xcrud->unset_remove(false);
