0a7161c7610a7ddee83184042297b7e69b2e2ac0
Soroush Yasini <soroush.yasini@outlook.com>
2025-02-01 13:52:51 +0330

    update TYPE presenting

--- processes/AminAmval/Xcrud.php
+++ processes/AminAmval/Xcrud.php
@@ -18,18 +18,19 @@ $xcrud->table('prc_db_amin_amval');
 // TYPE
 // VASZIYAT_ESTEFADE
 
-$xcrud->columns(array('id','GROUH', 'MAKAN', 'MOSHAKHASE', 'ONVAN_AMVAL', 'PELAK_AMVAL', 'SANAD', 'TAHVIL_GIRANDE', 'TARIKH', 'TEDAD', 'TYPE', 'VASZIYAT_ESTEFADE'));
+$xcrud->columns(array('id','GROUH', 'TEDAD', 'TYPE','MAKAN', 'ONVAN_AMVAL', 'MOSHAKHASE', 'PELAK_AMVAL', 'SANAD', 'TAHVIL_GIRANDE', 'TARIKH', 'VASZIYAT_ESTEFADE'));
 $xcrud->label('id','ID');
 $xcrud->label('GROUH','گروه کالا ');
+$xcrud->label('TEDAD','تعداد');
+$xcrud->label('TYPE',' نوع');
 $xcrud->label('MAKAN','مکان ');
-$xcrud->label('MOSHAKHASE','مشخصه');
 $xcrud->label('ONVAN_AMVAL',' عنوان اموال');
+$xcrud->label('MOSHAKHASE','مشخصه');
 $xcrud->label('PELAK_AMVAL','پلاک اموال');
 $xcrud->label('SANAD','سند');
 $xcrud->label('TAHVIL_GIRANDE',' تحویل گیرنده');
 $xcrud->label('TARIKH','تاریخ تحویل');
-$xcrud->label('TEDAD','تعداد');
-$xcrud->label('TYPE',' نوع');
+
 $xcrud->label('VASZIYAT_ESTEFADE','وضعیت استفاده');
 // Set your table or view from main database (you can write your query directly)
 $xcrud->unset_numbers();
@@ -42,4 +43,5 @@ $xcrud->button('javascript:;', 'Delete', 'fa fa-trash', 'btn btn-danger', array(
 $xcrud->unset_remove(false);
 $xcrud->unset_edit(false);
 $xcrud->unset_numbers();
+$xcrud->unset_csv(false);
 @@panel_grid = $xcrud->render();
