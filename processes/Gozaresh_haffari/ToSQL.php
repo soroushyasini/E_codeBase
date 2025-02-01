f1ab0e54f1262a72c5252da0b45d834ad2f152e1
Soroush Yasini <soroush.yasini@outlook.com>
2025-02-01 11:50:39 +0330

    some modifs to haffri e.g stop time is inclued

--- processes/Gozaresh_haffari/ToSQL.php
+++ processes/Gozaresh_haffari/ToSQL.php
@@ -40,6 +40,11 @@ $list_khruji = @@list_khruji;
 $text_checkbox_tozihat = @@text_checkbox_tozihat;
 $text_sharh_haffari = @@text_sharh_haffari;
 
+$is_stopped = @@is_stopped;
+$stop_causes = @@stop_causes;
+$stop_time = @@stop_time;
+$lv_pack = @@lv_pack;
+
 // Convert selected values to comma-separated strings
 $sarparastString = implode(',', $sarparastSelected);
 $negahbanString = implode(',', $negahbanSelected);
@@ -65,7 +70,7 @@ $query = "INSERT INTO prc_db_gozaresh_ruzane (
     gaso_flt, oil_flt, supermix_flt, bentonite_flt, sarparast, negahban, zaminshenas, 
     driver, sar_haffar, haffar, kargar, komak_haffar, aux_kargar, aux_komak_haffar, 
     list_vorudi, list_khruji, text_checkbox_tozihat, text_sharh_haffari, 
-    check_1_6_checkgroup, check_7_14_checkgroup, soda_flt, cement_flt
+    check_1_6_checkgroup, check_7_14_checkgroup, soda_flt, cement_flt, is_stopped, stop_causes, stop_time, lv_pack
 ) VALUES (
     '$Projects', '$form_serial_number_str', '$Gamane_name', '$dastgah_name', '$shift', DATE(n2_date('$form_date')), 
     '$dastgah_saat', '$start_our_str', '$end_our_str', $drill_start_flt, $drill_end_flt, 
@@ -73,11 +78,9 @@ $query = "INSERT INTO prc_db_gozaresh_ruzane (
     $gaso_flt, $oil_flt, $supermix_flt, $bentonite_flt, '$sarparastString', '$negahbanString', '$zaminshenasString', 
     '$driverString', '$sarHaffarString', '$haffarString', '$kargarString', '$komakHaffarString', 
     '$aux_kargar', '$aux_komak_haffar', '$list_vorudi', '$list_khruji', '$text_checkbox_tozihat', 
-    '$text_sharh_haffari', '$check_1_6_checkgroupString', '$check_7_14_checkgroupString', '$soda_flt', '$cement_flt'
+    '$text_sharh_haffari', '$check_1_6_checkgroupString', '$check_7_14_checkgroupString', '$soda_flt', '$cement_flt', '$is_stopped', '$stop_causes', '$stop_time', '$lv_pack'
 );";
 
-ADD soda_flt FLOAT,
-ADD cement_flt FLOAT;
 
 echo $query; // Debug: Print the query
 
