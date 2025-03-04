9dcd981ef02ec001fab4b28d349299f079ef4893
Soroush Yasini <soroush.yasini@outlook.com>
2025-03-04 10:26:14 +0330

    today init

---
+++ processes/amar_mahane_madan/variables.php
@@ -0,0 +1,10 @@
+time, name_madan, mah, sal, water, bargh, niro_ensani, gheymat_tolid,
+gheymat_forush, taghir_radio, taghirat, 
+
+meghdar_bateleh_1, nerkh_bateleh_1, mablagh_bateleh_1, tozihat_bateleh_1,
+meghdar_estekhraj_2, nerkh_estekhraj_2, mablagh_estekhraj_2, tozihat_estekhraj_2,
+meghdar_hamlshodeh_3, nekh_hamlshodeh_3, mablagh_hamlshodeh_3, tozihat_hamlshodeh_3,
+meghdar_khordshode_4, nerkh_khordshode_4, mablagh_khordshode_4, tozihat_khordshode_4,
+meghdar_khordNashode_5, nerkh_khordNashode_5, mablagh_khordNashode_5, tozihat_khordNashode_5,
+meghdar_forush_6, nerkh_forush_6, mablagh_forush_6, tozihat_forush_6,
+meghdar_depo_7, nerkh_depo_7, mablagh_depo_7, tozihat_depo_7
